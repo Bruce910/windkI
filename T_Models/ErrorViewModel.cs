e23932a5e05daee2cae5365f8fc1266d1da2c117
HAO <bruce8920@gmail.com>
2024-10-17 13:49:39 +0800

    資料庫進行調整

--- T_Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Final10._14.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
