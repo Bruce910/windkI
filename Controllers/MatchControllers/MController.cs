cb734def755ae53a718c2ec72355d5e6d72e8ed3
HAO <bruce8920@gmail.com>
2024-10-17 16:27:19 +0800

    完整修復了

--- Controllers/MatchControllers/MController.cs
+++ Controllers/MatchControllers/MController.cs
@@ -1,5 +1,6 @@
 ﻿using Final10._14.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 
 namespace Final10._14.Controllers.MatchControllers
 {
@@ -19,8 +20,52 @@ namespace Final10._14.Controllers.MatchControllers
 
         public IActionResult Create()
         {
+            
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(TProduct p)
+        {
+            _context.TProducts.Add(p);
+            _context.SaveChanges();
+            return RedirectToAction("List");
+        }
 
+        public IActionResult Edit(int id)
+        {
+            TMatch p = _context.TMatches.FirstOrDefault(x => x.FMatchId == id);
+            if(p==null)
+                return RedirectToAction("List");
+            return View(p);
+        }
+        [HttpPost]
+        public IActionResult Edit(TMatch p)
+        {
+            TMatch p1 = _context.TMatches.FirstOrDefault(x => x.FMatchId == p.FMatchId);
+            if(p1==null)
+                return RedirectToAction("List");
+           
+            
+            p1.FMatchDateTime = p.FMatchDateTime;
+            p1.FMemberId = p.FMemberId;
+            p1.FMatchDateTime = p.FMatchDateTime;
+            p1.FPoint = p.FPoint;
+            p1.FMatchStatus = p.FMatchStatus;
+            p1.FGradeDateTime=p.FGradeDateTime;
+            p1.FMessage = p.FMessage;
+            _context.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            TMatch p = _context.TMatches.FirstOrDefault(x => x.FMatchId == id);
+            if(p==null)
+                return RedirectToAction("List");
+            _context.TMatches.Remove(p);
+            _context.SaveChanges();
+            return RedirectToAction("List");
+        }
     }
 }
